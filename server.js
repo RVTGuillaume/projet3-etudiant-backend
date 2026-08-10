af06da27de260355ba7a484dfc09b7e072875b85
RVTGuillaume <rvonytianaguillaume@gmail.com>
2026-08-10 14:16:52 +0300

    Debug: affichage des routes et port dynamique

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 ﻿const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 // Route de base
 app.get('/', (req, res) => {
   res.json({ message: 'API Etudiant - operationnelle' });
@@ -16,7 +18,15 @@ app.get('/api/etudiants', (req, res) => {
   res.json({ success: true, data: [] });
 });
 
-const PORT = process.env.PORT || 3000;
+// Afficher toutes les routes enregistrées
+console.log('Routes disponibles :');
+app._router.stack.forEach((layer) => {
+  if (layer.route) {
+    console.log(layer.route.path);
+  }
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Serveur demarre sur le port ' + PORT);
+  console.log('URL: http://0.0.0.0:' + PORT);
 });
