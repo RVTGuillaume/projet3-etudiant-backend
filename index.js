0c7d37ceec9d70621ea161b2a4ea9420b619fd62
RVTGuillaume <rvonytianaguillaume@gmail.com>
2026-08-10 14:18:37 +0300

    Utilisation de index.js comme point d'entree

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const app = express();
+
+// Route de base
+app.get('/', (req, res) => {
+  res.json({ message: 'API Etudiant - operationnelle' });
+});
+
+// Route de test
+app.get('/test', (req, res) => {
+  res.json({ message: 'Route test fonctionne !' });
+});
+
+// Route API étudiants
+app.get('/api/etudiants', (req, res) => {
+  res.json({ success: true, data: [] });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Serveur demarre sur le port ' + PORT);
+});
