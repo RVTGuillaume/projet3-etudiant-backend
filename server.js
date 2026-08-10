1075f46c55c1b174260d764693dbc3a816162ab0
RVTGuillaume <rvonytianaguillaume@gmail.com>
2026-08-10 14:47:28 +0300

    Retour a Express avec CORS

--- server.js
+++ server.js
@@ -1,40 +1,24 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-  // Gestion des routes
-  if (req.url === '/') {
-    res.end(JSON.stringify({
-      message: 'API Etudiant - operationnelle',
-      routes: ['/api/etudiants', '/api/etudiants/:id']
-    }));
-  }
-  else if (req.url === '/api/etudiants') {
-    res.end(JSON.stringify({
-      success: true,
-      data: [],
-      message: 'Route /api/etudiants fonctionne !'
-    }));
-  }
-  else if (req.url.startsWith('/api/etudiants/')) {
-    const id = req.url.split('/').pop();
-    res.end(JSON.stringify({
-      success: true,
-      id: id,
-      message: 'Route /api/etudiants/' + id + ' fonctionne !'
-    }));
-  }
-  else {
-    res.statusCode = 404;
-    res.end(JSON.stringify({
-      error: 'Route non trouv?e',
-      path: req.url
-    }));
-  }
+app.get('/', (req, res) => {
+  res.json({ message: 'API Etudiant - operationnelle' });
+});
+
+app.get('/api/etudiants', (req, res) => {
+  res.json({ success: true, data: [] });
+});
+
+app.post('/api/etudiants', (req, res) => {
+  res.json({ success: true, message: 'Etudiant ajoute' });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log('Serveur HTTP demarre sur le port ' + PORT);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Serveur Express demarre sur le port ' + PORT);
 });
