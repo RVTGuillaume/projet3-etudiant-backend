55abc90540b63b8871e732c411095b4b1e888e63
RVTGuillaume <rvonytianaguillaume@gmail.com>
2026-08-10 14:38:49 +0300

    Ajout route /api/etudiants dans serveur HTTP

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ 
-    message: 'Test serveur HTTP fonctionne !',
-    path: req.url,
-    method: req.method
-  }));
+  
+  // Route /api/etudiants
+  if (req.url === '/api/etudiants') {
+    res.end(JSON.stringify({ 
+      success: true, 
+      data: [],
+      message: 'Route API fonctionne !'
+    }));
+  } else {
+    // Route racine
+    res.end(JSON.stringify({ 
+      message: 'Test serveur HTTP fonctionne !',
+      path: req.url,
+      method: req.method
+    }));
+  }
 });
 
 const PORT = process.env.PORT || 3000;
